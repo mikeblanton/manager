1d8e70048d366800c54f831e641256719c2aa914
Michael Blanton <mblanton@gmail.com>
2017-09-21 17:09:46 -0700

    Added email entry to Redux support

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 // TODO: Rewrite using FormInput (see auth)
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
       <Card>
@@ -11,6 +17,8 @@ class LoginForm extends Component {
           <Input
             label="Email"
             placeholder="email@mail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
           />
         </CardSection>
         <CardSection>
@@ -30,4 +38,10 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
