28cf18ea07df372ab5232031b68345b0672986d2
Michael Blanton <mblanton@gmail.com>
2017-09-22 19:23:00 -0700

    Added initial employee list and create code

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Button } from './common';
+
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            lable="Phone"
+            placeholder="555-555-5555"
+          />
+        </CardSection>
+        <CardSection>
+
+        </CardSection>
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default EmployeeCreate;
